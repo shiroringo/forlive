b8af96faf12f33f024415aec7bf0fa56d3ade61b
ringo <ringo@miaopuc.com>
2024-06-02 20:28:40 +0800

    feat: 添加时钟

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,9 +8,10 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      666
+      <!-- You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next? -->
     </h3>
   </div>
 </template>
